c1042f58e72b41619413f6938fe4a33ffd1b7534
DukeTime <duke.of.times@gmail.com>
2024-10-05 14:34:36 +0300

    visual1

---
+++ Assets/Code/Scripts/G.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G : MonoBehaviour
+{
+    public static int money = 0;
+    public static int unitCount = 0;
+    public static int unitLvl = 0;
+    public static int lvl = 0;
+
+    public static GameObject[] ObjectsOnStage;
+
+    //[SerializeField] private 
+}
---
+++ Assets/Code/Scripts/UnitController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    public enum unitStage {spawn, idle, fight};
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
